ba90185c7c718c2e88324be2e96081bfd18f27e6
Timur <timuruteev007@gmail.com>
2024-02-20 15:16:09 +0300

    до функционала

---
+++ Sleepy/TimerView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerView.swift
+//  Sleepy
+//
+//  Created by Timur on 20.02.2024.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerView()
+}
