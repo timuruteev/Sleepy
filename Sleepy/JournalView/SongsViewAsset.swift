5e41f6ce84558335d7d08210aa23a7333c5f5a93
Timur <timuruteev007@gmail.com>
2024-02-27 13:53:19 +0300

    рабочие запись звуков и их воспроизведение

---
+++ Sleepy/JournalView/SongsViewAsset.swift
@@ -0,0 +1,18 @@
+//
+//  SongsViewAsset.swift
+//  Sleepy
+//
+//  Created by Timur on 27.02.2024.
+//
+
+import SwiftUI
+
+struct SongsViewAsset: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SongsViewAsset()
+}
