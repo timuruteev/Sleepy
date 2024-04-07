f075527dc3e34bda665b82261d92019ec77ce906
Timur <timuruteev007@gmail.com>
2024-04-07 15:54:10 +0300

    убрал элементы с качеством сна в окне статистика и профиль, в профиле заменил "период пробуждения" на "период засыпания" + добавил заголовок "записанные звуки" в окне со звуками

---
+++ Sleepy/ProfileView/TimeToSleep.swift
@@ -0,0 +1,18 @@
+//
+//  TimeToSleep.swift
+//  Sleepy
+//
+//  Created by Timur on 07.04.2024.
+//
+
+import SwiftUI
+
+struct TimeToSleep: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeToSleep()
+}
