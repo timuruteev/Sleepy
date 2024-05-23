4d7c5ea613ab44e5d5afad63fab3bee6ccff5765
Timur <timuruteev007@gmail.com>
2024-05-23 12:45:45 +0300

    добавление в настройки новый параметр: повтор будильника

---
+++ Sleepy/ProfileView/SnoozeViewAsset.swift
@@ -0,0 +1,18 @@
+//
+//  SnoozeViewAsset.swift
+//  Sleepy
+//
+//  Created by Timur on 23.05.2024.
+//
+
+import SwiftUI
+
+struct SnoozeViewAsset: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SnoozeViewAsset()
+}
