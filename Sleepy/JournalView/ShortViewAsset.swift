64cf090378a676e72548248728c13028e95c381e
Timur <timuruteev007@gmail.com>
2024-06-09 15:19:45 +0300

    сделал смену звука будильника и его воспроизведение на физическом айфоне (было только на эмуляторе)

--- Sleepy/JournalView/ShortViewAsset.swift
+++ Sleepy/JournalView/ShortViewAsset.swift
@@ -5,17 +5,21 @@ struct ShortViewAsset: SwiftUI.View {
     @SwiftUI.Binding var selectedDate: Date
     @State private var startTime: String = "00:00"
     @State private var endTime: String = "00:00"
+    private var db: Connection?
 
     init(selectedDate: SwiftUI.Binding<Date>) {
         self._selectedDate = selectedDate
-        self.copyDatabaseIfNeeded()
+        self.copyDatabaseAndSoundsIfNeeded()
+        self.db = self.openDatabase()
     }
 
-    func copyDatabaseIfNeeded() {
+    func copyDatabaseAndSoundsIfNeeded() {
         let fileManager = FileManager.default
         let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
         let finalDatabaseURL = documentsDirectory.appendingPathComponent("Sleepy1.db")
+        let finalSoundsDirectory = documentsDirectory.appendingPathComponent("sounds")
 
+        // Copy database if it doesn't exist
         if !fileManager.fileExists(atPath: finalDatabaseURL.path) {
             if let bundleDatabaseURL = Bundle.main.url(forResource: "Sleepy1", withExtension: "db") {
                 do {
@@ -30,56 +34,120 @@ struct ShortViewAsset: SwiftUI.View {
         } else {
             print("Database already exists at path: \(finalDatabaseURL.path)")
         }
+
+        // Copy sounds folder if it doesn't exist
+        if !fileManager.fileExists(atPath: finalSoundsDirectory.path) {
+            if let bundleSoundsURL = Bundle.main.url(forResource: "sounds", withExtension: nil) {
+                do {
+                    try fileManager.createDirectory(at: finalSoundsDirectory, withIntermediateDirectories: true, attributes: nil)
+                    let soundFiles = try fileManager.contentsOfDirectory(at: bundleSoundsURL, includingPropertiesForKeys: nil, options: [])
+                    for file in soundFiles {
+                        let destinationURL = finalSoundsDirectory.appendingPathComponent(file.lastPathComponent)
+                        try fileManager.copyItem(at: file, to: destinationURL)
+                    }
+                    print("Sounds directory copied to documents directory at path: \(finalSoundsDirectory.path)")
+                } catch {
+                    print("Error copying sounds directory: \(error)")
+                }
+            } else {
+                print("Sounds directory not found in bundle")
+            }
+        } else {
+            print("Sounds directory already exists at path: \(finalSoundsDirectory.path)")
+        }
     }
 
-    func fetchSleepData() {
+    func openDatabase() -> Connection? {
         let fileManager = FileManager.default
         let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
         let finalDatabaseURL = documentsDirectory.appendingPathComponent("Sleepy1.db")
 
         do {
-            let db = try Connection(finalDatabaseURL.path, readonly: true)
+            let db = try Connection(finalDatabaseURL.path)
             print("Database opened at path: \(finalDatabaseURL.path)")
+            return db
+        } catch {
+            print("Error opening database: \(error)")
+            return nil
+        }
+    }
 
-            let tables = try db.scalar("SELECT name FROM sqlite_master WHERE type='table' AND name='Statistic';")
-            print("Table 'Statistic' exists: \(tables)")
+    func fetchSleepData() {
+        guard let db = db else { return }
 
-            let statistic = Table("Statistic")
-            let idAlarm = Expression<Int64>("IdAlarm")
-            let dateAlarmExpr = Expression<String>("DateAlarm")
-            let startTimeExpr = Expression<String>("StartTime")
-            let endTimeExpr = Expression<String>("EndTime")
+        let statistic = Table("Statistic")
+        let idAlarm = Expression<Int64>("IdAlarm")
+        let dateAlarmExpr = Expression<String>("DateAlarm")
+        let startTimeExpr = Expression<String>("StartTime")
+        let endTimeExpr = Expression<String>("EndTime")
 
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyy-MM-dd"
-            let selectedDateString = dateFormatter.string(from: selectedDate)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let selectedDateString = dateFormatter.string(from: selectedDate)
 
-            let timeFormatter = DateFormatter()
-            timeFormatter.dateFormat = "HH:mm:ss"
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = "HH:mm:ss"
 
-            let displayTimeFormatter = DateFormatter()
-            displayTimeFormatter.dateFormat = "HH:mm"
+        let displayTimeFormatter = DateFormatter()
+        displayTimeFormatter.dateFormat = "HH:mm"
 
-            let query = statistic.select(startTimeExpr, endTimeExpr)
-                                 .where(dateAlarmExpr == selectedDateString)
-                                 .order(idAlarm.desc)
-                                 .limit(1)
-            do {
-                if let row = try db.pluck(query) {
-                    if let startTimeDate = timeFormatter.date(from: row[startTimeExpr]),
-                       let endTimeDate = timeFormatter.date(from: row[endTimeExpr]) {
-                        self.startTime = displayTimeFormatter.string(from: startTimeDate)
-                        self.endTime = displayTimeFormatter.string(from: endTimeDate)
-                    }
-                } else {
-                    self.startTime = "Нет данных"
-                    self.endTime = "Нет данных"
+        let query = statistic.select(startTimeExpr, endTimeExpr)
+                             .where(dateAlarmExpr == selectedDateString)
+                             .order(idAlarm.desc)
+                             .limit(1)
+        do {
+            if let row = try db.pluck(query) {
+                if let startTimeDate = timeFormatter.date(from: row[startTimeExpr]),
+                   let endTimeDate = timeFormatter.date(from: row[endTimeExpr]) {
+                    self.startTime = displayTimeFormatter.string(from: startTimeDate)
+                    self.endTime = displayTimeFormatter.string(from: endTimeDate)
                 }
-            } catch {
-                print("Ошибка при выборке данных: \(error)")
+            } else {
+                self.startTime = "Нет данных"
+                self.endTime = "Нет данных"
             }
         } catch {
-            print("Ошибка при открытии базы данных: \(error)")
+            print("Ошибка при выборке данных: \(error)")
+        }
+    }
+
+    func insertSleepData(startTime: String, endTime: String, dateAlarm: String) {
+        guard let db = db else { return }
+
+        let statistic = Table("Statistic")
+        let dateAlarmExpr = Expression<String>("DateAlarm")
+        let startTimeExpr = Expression<String>("StartTime")
+        let endTimeExpr = Expression<String>("EndTime")
+
+        let insert = statistic.insert(dateAlarmExpr <- dateAlarm, startTimeExpr <- startTime, endTimeExpr <- endTime)
+
+        do {
+            try db.run(insert)
+            print("Inserted sleep data")
+        } catch {
+            print("Ошибка при вставке данных: \(error)")
+        }
+    }
+
+    func updateSleepData(startTime: String, endTime: String, dateAlarm: String) {
+        guard let db = db else { return }
+
+        let statistic = Table("Statistic")
+        let dateAlarmExpr = Expression<String>("DateAlarm")
+        let startTimeExpr = Expression<String>("StartTime")
+        let endTimeExpr = Expression<String>("EndTime")
+
+        let row = statistic.filter(dateAlarmExpr == dateAlarm)
+        let update = row.update(startTimeExpr <- startTime, endTimeExpr <- endTime)
+
+        do {
+            if try db.run(update) > 0 {
+                print("Updated sleep data")
+            } else {
+                print("No data found to update")
+            }
+        } catch {
+            print("Ошибка при обновлении данных: \(error)")
         }
     }
 
