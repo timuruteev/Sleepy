15ece80d50ae3881398c49fb75b2d80b477ad99a
Timur <timuruteev007@gmail.com>
2024-06-16 17:27:56 +0300

    вывод время в постели и во сне из здоровья

--- Sleepy/JournalView/QualityViewAsset.swift
+++ Sleepy/JournalView/QualityViewAsset.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import HealthKit
 import SQLite
 
 struct QualityViewAsset: SwiftUI.View {
@@ -7,85 +8,86 @@ struct QualityViewAsset: SwiftUI.View {
     @State private var endTime: String = "00:00"
     @State private var timeInBed: String = "0ч 0мин"
     @State private var timeAsleep: String = "0ч 0мин"
+    private let healthStore = HKHealthStore()
+
+    func requestAuthorization() {
+        let typesToShare: Set = Set(arrayLiteral: HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!)
+        let typesToRead: Set = Set(arrayLiteral: HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!)
+
+        healthStore.requestAuthorization(toShare: typesToShare, read: typesToRead) { (success, error) in
+            if !success {
+                print("Authorization failed: \(error?.localizedDescription ?? "Unknown error")")
+            }
+        }
+    }
 
     func fetchSleepData() {
+        let calendar = Calendar.current
+        let startOfDay = calendar.startOfDay(for: selectedDate)
+        let endOfDay = calendar.date(byAdding: .day, value: 1, to: startOfDay)!
+
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: endOfDay, options: .strictStartDate)
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+        
+        let query = HKSampleQuery(sampleType: HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!, predicate: predicate, limit: 1, sortDescriptors: [sortDescriptor]) { query, results, error in
+            guard let result = results?.first as? HKCategorySample else {
+                DispatchQueue.main.async {
+                    self.startTime = "Нет данных"
+                    self.endTime = "Нет данных"
+                    self.timeInBed = "Нет данных"
+                    self.timeAsleep = "Нет данных"
+                }
+                return
+            }
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "HH:mm"
+            
+            DispatchQueue.main.async {
+                self.startTime = formatter.string(from: result.startDate)
+                self.endTime = formatter.string(from: result.endDate)
+                
+                // Вычисление времени в постели
+                let components = calendar.dateComponents([.hour, .minute], from: result.startDate, to: result.endDate)
+                if let hours = components.hour, let minutes = components.minute {
+                    self.timeInBed = "\(hours)ч \(minutes)мин"
+                }
+
+                // Получение времени сна из БД
+                self.fetchSleepDuration { duration in
+                    let sleepDuration = duration * 60 // duration in seconds
+                    let timeDifference = result.endDate.timeIntervalSince(result.startDate)
+                    if timeDifference >= Double(sleepDuration) {
+                        let adjustedEndTimeForSleep = result.endDate.addingTimeInterval(-Double(sleepDuration))
+                        let sleepComponents = calendar.dateComponents([.hour, .minute], from: result.startDate, to: adjustedEndTimeForSleep)
+                        if let sleepHours = sleepComponents.hour, let sleepMinutes = sleepComponents.minute {
+                            self.timeAsleep = "\(sleepHours)ч \(sleepMinutes)мин"
+                        }
+                    } else {
+                        self.timeAsleep = "0ч 0мин"
+                    }
+                }
+            }
+        }
+
+        healthStore.execute(query)
+    }
+
+    func fetchSleepDuration(completion: @escaping (Int) -> Void) {
         let fileManager = FileManager.default
         let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
         let finalDatabaseURL = documentsDirectory.appendingPathComponent("Sleepy1.db")
 
         let db = try! Connection(finalDatabaseURL.path, readonly: true)
 
-        let statistic = Table("Statistic")
         let sleepPeriodTable = Table("SleepPeriod")
-        let idAlarm = Expression<Int64>("IdAlarm")
-        let dateAlarmExpr = Expression<String>("DateAlarm")
-        let startTimeExpr = Expression<String>("StartTime")
-        let endTimeExpr = Expression<String>("EndTime")
         let durationExpr = Expression<Int>("Duration")
 
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        let selectedDateString = dateFormatter.string(from: selectedDate)
-
-        let timeFormatter = DateFormatter()
-        timeFormatter.dateFormat = "HH:mm:ss"
-
-        let displayTimeFormatter = DateFormatter()
-        displayTimeFormatter.dateFormat = "HH:mm"
-
-        let calendar = Calendar.current
-
-        let query = statistic.select(startTimeExpr, endTimeExpr)
-                             .where(dateAlarmExpr == selectedDateString)
-                             .order(idAlarm.desc)
-                             .limit(1)
-
         if let sleepPeriodRow = try? db.pluck(sleepPeriodTable) {
-            let sleepDuration = sleepPeriodRow[durationExpr] * 60
-            
-            do {
-                if let row = try db.pluck(query) {
-                    if var startTimeDate = timeFormatter.date(from: row[startTimeExpr]),
-                       var endTimeDate = timeFormatter.date(from: row[endTimeExpr]) {
-                        
-                        // Проверка, произошел ли переход через полночь
-                        if endTimeDate < startTimeDate {
-                            // Добавляем 24 часа к времени окончания, если оно раньше времени начала
-                            endTimeDate = calendar.date(byAdding: .day, value: 1, to: endTimeDate)!
-                        }
-
-                        self.startTime = displayTimeFormatter.string(from: startTimeDate)
-                        self.endTime = displayTimeFormatter.string(from: endTimeDate)
-
-                        // Вычисление времени в постели
-                        let components = calendar.dateComponents([.hour, .minute], from: startTimeDate, to: endTimeDate)
-                        if let hours = components.hour, let minutes = components.minute {
-                            self.timeInBed = "\(hours)ч \(minutes)мин"
-                        }
-
-                        // Вычисление времени во сне с учетом Duration из таблицы SleepPeriod
-                        let timeDifference = endTimeDate.timeIntervalSince(startTimeDate)
-                        if timeDifference >= Double(sleepDuration) {
-                            let adjustedEndTimeForSleep = endTimeDate.addingTimeInterval(-Double(sleepDuration))
-                            let sleepComponents = calendar.dateComponents([.hour, .minute], from: startTimeDate, to: adjustedEndTimeForSleep)
-                            if let sleepHours = sleepComponents.hour, let sleepMinutes = sleepComponents.minute {
-                                self.timeAsleep = "\(sleepHours)ч \(sleepMinutes)мин"
-                            }
-                        } else {
-                            self.timeAsleep = "0ч 0мин"
-                        }
-                    }
-                } else {
-                    self.startTime = "Нет данных"
-                    self.endTime = "Нет данных"
-                    self.timeInBed = "Нет данных"
-                    self.timeAsleep = "Нет данных"
-                }
-            } catch {
-                print("Ошибка при выборке данных: \(error)")
-            }
+            let sleepDuration = sleepPeriodRow[durationExpr]
+            completion(sleepDuration)
         } else {
-            self.timeAsleep = "Нет данных"
+            completion(0)
         }
     }
 
@@ -119,17 +121,19 @@ struct QualityViewAsset: SwiftUI.View {
         .frame(maxWidth: .infinity, alignment: .leading)
         Divider()
             .background(Color.gray)
-        .onAppear(perform: fetchSleepData)
+        .onAppear(perform: {
+            requestAuthorization()
+            fetchSleepData()
+        })
         .onChange(of: selectedDate) { _ in
             fetchSleepData()
         }
     }
 }
 
-struct QualityViewAsset_Previews : PreviewProvider{
-    static var previews:some SwiftUI.View{
+struct QualityViewAsset_Previews : PreviewProvider {
+    static var previews: some SwiftUI.View {
         QualityViewAsset(selectedDate: .constant(Date()))
             .background(Color.black)
-        
     }
 }
